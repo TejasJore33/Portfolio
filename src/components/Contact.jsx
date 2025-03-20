05e5807cff48e787d52ea299c4d2f6c509a2a85a
Tejas Jore <116269768+TejasJore33@users.noreply.github.com>
2025-03-20 19:46:47 +0530

    .

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -13,14 +13,14 @@ const Contact = () => {
       <h1 style={{ marginTop: '60px' }}>CONTACT ME</h1>
       <div className="contact-icon"
       data-aos="zoom-in-up" data-aos-duration="1000">
-        <a href='' target='_blank' className="items"><FaInstagram className='icons'/>
+        <a href='https://www.instagram.com/tejas__jore?igsh=eWRrNTR1MGhndTdl' target='_blank' className="items"><FaInstagram className='icons'/>
         </a>
         <a className="items"><CiFacebook className='icons'/></a>
-        <a className="items"><FaLinkedin className='icons'/>
+        <a href='https://www.linkedin.com/in/tejas-jore-207794254' target='_blank' className="items"><FaLinkedin className='icons'/>
         </a>
-        <a className="items"><FaXTwitter className='icons'/>
+        <a href='https://x.com/tejasjore33' target='_blank' className="items"><FaXTwitter className='icons'/>
         </a>
-        <a className="items"><FaGithub className='icons'/> 
+        <a href='https://github.com/TejasJore33'target='_blank' className="items"><FaGithub className='icons'/> 
         </a>
         <a href='mailto:tejasjore33@gmail.com' target='_blank' className="items"><SiGmail className='icons'/>
         </a>
