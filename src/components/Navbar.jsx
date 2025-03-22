3403a69271f98c83ac04387c1a9709f290842377
Tejas Jore <116269768+TejasJore33@users.noreply.github.com>
2025-03-22 10:15:43 +0530

    Updating the Navbar and index.css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,9 +7,9 @@ const Navbar = () => {
         <div className="left nav_items">Portfolio</div>
         <div className="right">
           <a href="#home" className="nav_items" aria-label="Go to Home Section">Home</a>
-          <a href="#experience" className="nav_items" aria-label="Go to Experience Section">Experience</a>
-          <a href="#skills" className="nav_items" aria-label="Go to Skills Section">Skills</a>
           <a href="#projects" className="nav_items" aria-label="Go to Projects Section">Project</a>
+          <a href="#skills" className="nav_items" aria-label="Go to Skills Section">Skills</a>
+          <a href="#experience" className="nav_items" aria-label="Go to Experience Section">Experience</a>
           <a href="#contact" className="nav_items" aria-label="Go to Contact Section">Contact</a>
         </div>
       </div>
